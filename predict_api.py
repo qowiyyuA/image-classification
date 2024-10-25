5277f2562eb2f3484572cf7bec696483d645e7f9
qowiyyuA <qowiyyuadzkar@gmail.com>
2024-10-26 03:34:47 +0700

    First Commit

---
+++ predict_api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+
+app = Flask(__name__)
+
+# Load the H5 model
+model = tf.keras.models.load_model('model/image_classification_model.h5')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    input_data = data.get('input')
+    prediction = model.predict([input_data])
+    prediction = prediction.tolist()  # Convert to list for JSON response
+
+    return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(port=5000)
