e17814036641e54cb72964678af2bfa625571e0a
qowiyyuA <qowiyyuadzkar@gmail.com>
2024-10-26 10:07:11 +0700

    change code predict

--- predict_api.py
+++ predict_api.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
-# Load the H5 model
-model = tf.keras.models.load_model('/image_classification_model.h5')
+model = tf.keras.models.load_model('image_classification_model.h5')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    input_data = data.get('input')
-    prediction = model.predict([input_data])
-    prediction = prediction.tolist()  # Convert to list for JSON response
+    # Periksa apakah ada file di request
+    if 'input' not in request.files:
+        return jsonify({"error": "File tidak ditemukan di permintaan"}), 400
+
+    # Ambil file gambar dari permintaan
+    file = request.files['input']
+    image = Image.open(file).resize((224, 224))  # Sesuaikan ukuran dengan input model
+    image = np.array(image) / 255.0  # Normalisasi jika perlu
+    image = np.expand_dims(image, axis=0)  # Tambahkan batch dimension
+
+    prediction = model.predict(image)  # Prediksi
+    prediction = prediction.tolist()
 
     return jsonify({'prediction': prediction})
 
